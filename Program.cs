c63f55b3e2c48aadd5bc63fa04cf1dd26511884e
Sasha <F712@rambler.ru>
2022-09-07 14:48:35 +0300

    Вторая задача с координатами

--- Program.cs
+++ Program.cs
@@ -1,6 +1,59 @@
 ﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), 
 //причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     return Convert.ToInt32 (value);
+// }
+
+// bool ValidateCoord(int xCoord , int yCoord)
+// {
+
+//     if (xCoord == 0 || yCoord == 0)
+//     {
+//          System.Console.WriteLine("одна из координат находиться на оси .");
+//          return false;
+//     }
+//     return true;
+// }
+
+// void FindQuarter(int x , int y)
+// {
+//        if (x > 0 && y > 0)
+//     {
+//         System.Console.WriteLine("первая четверть ");
+//     }
+//          if (x < 0 && y > 0)
+//     {
+//         System.Console.WriteLine("вторая четверть ");
+//     }
+//         if (x < 0 && y < 0)
+//     {
+//         System.Console.WriteLine("третья четверть ");
+//     }
+//          if (x > 0 && y < 0)
+//     {
+//         System.Console.WriteLine("четвертая четверть ");
+//     }
+// }
+
+// int xCoord = Prompt ("Введите Х : ");
+// int yCoord = Prompt ("Введите Y : ");
+
+// if (ValidateCoord(xCoord,yCoord))
+// {
+//     FindQuarter(xCoord, yCoord);
+// }
+
+//End
+
+
+
+//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
 int Prompt(string message)
 {
     System.Console.Write(message);
@@ -8,41 +61,41 @@ int Prompt(string message)
     return Convert.ToInt32 (value);
 }
 
-bool ValidateCoord(int xCoord , int yCoord)
+bool ValidateQuarter(int Quarter)
 {
-
-    if (xCoord == 0 || yCoord == 0)
+    if (Quarter > 0 && Quarter < 5)
     {
-         System.Console.WriteLine("одна из координат находиться на оси .");
-         return false;
+         return true;
     }
-    return true;
+         System.Console.WriteLine("ошибка ввода");
+    return false;
 }
 
-void FindQuarter(int x , int y)
+void GetQuarter(int Quarter)
 {
-       if (x > 0 && y > 0)
+       if (Quarter == 1)
     {
-        System.Console.WriteLine("первая четверть ");
+        System.Console.WriteLine("x > 0 & y > 0 ");
     }
-         if (x < 0 && y > 0)
+         if (Quarter == 2)
     {
-        System.Console.WriteLine("вторая четверть ");
+        System.Console.WriteLine("x < 0 & y > 0  ");
     }
-        if (x < 0 && y < 0)
+        if (Quarter == 3)
     {
-        System.Console.WriteLine("третья четверть ");
+        System.Console.WriteLine("x < 0 & y < 0 ");
     }
-         if (x > 0 && y < 0)
+         if (Quarter == 4)
     {
-        System.Console.WriteLine("четвертая четверть ");
+        System.Console.WriteLine("x > 0 & y < 0 ");
     }
 }
 
-int xCoord = Prompt ("Введите Х : ");
-int yCoord = Prompt ("Введите Y : ");
+int Q = Prompt ("Введите номер четверти  : ");
 
-if (ValidateCoord(xCoord,yCoord))
+if (ValidateQuarter(Q))
 {
-    FindQuarter(xCoord, yCoord);
+    GetQuarter(Q);
 }
+
+// End
